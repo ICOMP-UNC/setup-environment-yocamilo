f297587a900385aa8c5678644f9d85762de7d7c9
yocamilo <camilo.gorriti@mi.unc.edu.ar>
2024-03-21 20:46:05 -0300

    Add executable

---
+++ src/functions.c
@@ -0,0 +1,7 @@
+#include "functions.h"
+#include <stdio.h>
+
+void functions()
+{
+    printf("Utilizando funcion\n");
+}
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <functions.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello from main\n");
+
+    functions();
+
+    return 0;
+}

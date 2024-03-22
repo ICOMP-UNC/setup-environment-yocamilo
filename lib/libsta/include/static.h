df1e1bf186fa70c1332c09a0d76cf6d90a46d2c4
yocamilo <camilo.gorriti@mi.unc.edu.ar>
2024-03-21 21:54:44 -0300

    [1.1.0]
    Add and link librarys

---
+++ lib/libsta/include/static.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+* static_lib_function
+*
+* This function is defied in the static library and is program.
+*
+* @param msg: The message to be printed.
+* @return The message size.
+*/
+
+int static_lib_function(char* msg);

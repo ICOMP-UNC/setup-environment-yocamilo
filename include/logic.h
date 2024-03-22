806a2610a575379ecd24a835f80f015b13785904
yocamilo <camilo.gorriti@mi.unc.edu.ar>
2024-03-21 23:30:29 -0300

    [1.2.0]
    Unit Tests and IC

---
+++ include/logic.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/**
+ * @brief Add two numbers.
+ * 
+ * @param first_number First number
+ * @param second_number Second number
+ * @return first_number + second_number
+ */
+int add(int first_number, int second_number);
+
+/**
+ * @brief Sustract two numbers.
+ * 
+ * @param fist_number First number
+ * @param second_number Second number
+ * @return first_number - second_number
+ */
+int subtract(int first_number, int second_number);
+
+/**
+ * @brief Multiply two numbers
+ * 
+ * @param fist_number First number
+ * @param second_number Second number
+ * @return first_number * second_number
+ */
+int multiply(int first_number, int second_number);
+
+/**
+ * @brief Divide two numbers
+ * 
+ * @param fist_number First number
+ * @param second_number Second number
+ * @return first_number / second_number if second_number != 0
+ */
+double divide(int first_number, int second_number);
